a5e3782ee8b89f95b6085b6ee051c75d56484304
ayylmaoRotE <pedro.h.soares@live.com>
2025-08-27 18:56:44 +1000

    Added chance based conversion

--- src/New/Type/Affiliated/TypeConvertGroup.cpp
+++ src/New/Type/Affiliated/TypeConvertGroup.cpp
@@ -1,15 +1,20 @@
 #include <Ext/Techno/Body.h>
 #include <AnimClass.h>
+#include <ScenarioClass.h>
 #include "TypeConvertGroup.h"
 
 void TypeConvertGroup::Convert(FootClass* pTargetFoot, const std::vector<TypeConvertGroup>& convertPairs, HouseClass* pOwner, AnimTypeClass* pConvertAnim)
 {
-	for (const auto& [fromTypes, toType, affectedHouses] : convertPairs)
+	for (const auto& [fromTypes, toType, affectedHouses, chance] : convertPairs)
 	{
 		if (!toType.Get())
 			continue;
 
 		if (pOwner && !EnumFunctions::CanTargetHouse(affectedHouses, pOwner, pTargetFoot->Owner))
+			continue;
+
+		// Check conversion chance using deterministic game RNG
+		if (ScenarioClass::Instance->Random.RandomDouble() > chance.Get())
 			continue;
 
 		if (fromTypes.size())
@@ -87,12 +92,15 @@ void TypeConvertGroup::Parse(std::vector<TypeConvertGroup>& list, INI_EX& exINI,
 		ValueableVector<TechnoTypeClass*> convertFrom;
 		Nullable<TechnoTypeClass*> convertTo;
 		Nullable<AffectedHouse> convertAffectedHouses;
+		Valueable<double> convertChance { 1.0 };
 		_snprintf_s(tempBuffer, sizeof(tempBuffer), "Convert%d.From", i);
 		convertFrom.Read(exINI, pSection, tempBuffer);
 		_snprintf_s(tempBuffer, sizeof(tempBuffer), "Convert%d.To", i);
 		convertTo.Read(exINI, pSection, tempBuffer);
 		_snprintf_s(tempBuffer, sizeof(tempBuffer), "Convert%d.AffectedHouses", i);
 		convertAffectedHouses.Read(exINI, pSection, tempBuffer);
+		_snprintf_s(tempBuffer, sizeof(tempBuffer), "Convert%d.Chance", i);
+		convertChance.Read(exINI, pSection, tempBuffer);
 
 		if (!convertTo.isset())
 			break;
@@ -100,23 +108,25 @@ void TypeConvertGroup::Parse(std::vector<TypeConvertGroup>& list, INI_EX& exINI,
 		if (!convertAffectedHouses.isset())
 			convertAffectedHouses = defaultAffectHouse;
 
-		list.emplace_back(convertFrom, convertTo, convertAffectedHouses);
+		list.emplace_back(convertFrom, convertTo, convertAffectedHouses, convertChance.Get());
 	}
 	ValueableVector<TechnoTypeClass*> convertFrom;
 	Nullable<TechnoTypeClass*> convertTo;
 	Nullable<AffectedHouse> convertAffectedHouses;
+	Valueable<double> convertChance { 1.0 };
 	convertFrom.Read(exINI, pSection, "Convert.From");
 	convertTo.Read(exINI, pSection, "Convert.To");
 	convertAffectedHouses.Read(exINI, pSection, "Convert.AffectedHouses");
+	convertChance.Read(exINI, pSection, "Convert.Chance");
 	if (convertTo.isset())
 	{
 		if (!convertAffectedHouses.isset())
 			convertAffectedHouses = defaultAffectHouse;
 
 		if (list.size())
-			list[0] = { convertFrom, convertTo, convertAffectedHouses };
+			list[0] = { convertFrom, convertTo, convertAffectedHouses, convertChance.Get() };
 		else
-			list.emplace_back(convertFrom, convertTo, convertAffectedHouses);
+			list.emplace_back(convertFrom, convertTo, convertAffectedHouses, convertChance.Get());
 	}
 }
 
@@ -127,5 +137,6 @@ bool TypeConvertGroup::Serialize(T& stm)
 		.Process(this->FromTypes)
 		.Process(this->ToType)
 		.Process(this->AppliedTo)
+		.Process(this->Chance)
 		.Success();
 }
